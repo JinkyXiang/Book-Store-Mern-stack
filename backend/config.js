ba05afbb117376f7fcd2eb4788f009928d1631d7
JinkyXiang <109024403+JinkyXiang@users.noreply.github.com>
2024-05-22 21:46:59 +1200

    connect to mongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,6 @@
 //define port
 export const PORT = 3000;
+
+//define MongoDB URL
+export const mongoDBUrl =
+  "mongodb+srv://root:root@book-store-mern.7cysppp.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN";
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 // Initiate the express server
 import express, { response } from "express";
-import { PORT } from "./config.js"; //import PORT from config.js
+import { PORT, mongoDBUrl } from "./config.js"; //import PORT from config.js
+import mongoose from "mongoose"; //import mongoose
 
 //create an instance of express
 const app = express();
@@ -11,7 +12,16 @@ app.get("/", (req, res) => {
   return res.status(234).send("Hello World, welcome to MERN stack");
 });
 
-//function to listen to the port
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(mongoDBUrl)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    //only when connect to mongo, function to listen to the port
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB");
+    console.log(error);
+  });
