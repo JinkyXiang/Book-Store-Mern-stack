d87a1d7ee8d3095bba761285185ab6bf41d6d2d4
JinkyXiang <109024403+JinkyXiang@users.noreply.github.com>
2024-05-23 16:54:00 +1200

    frontend vite, route setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
+import CreateBook from "./pages/CreateBook";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/book/details/:id" element={<ShowBook />} />
+      <Route path="/book/create" element={<CreateBook />} />
+      <Route path="/book/edit/:id" element={<EditBook />} />
+      <Route path="/book/delete/:id" element={<DeleteBook />} />
+    </Routes>
+  );
+};
+
+export default App;
